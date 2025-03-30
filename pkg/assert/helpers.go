c499301acf88d1145e198f738b5740b857bfa6c3
Zhenya Khvan <zhenya.khvan@gmail.com>
2025-03-30 09:19:16 -0400

    test: assert helpers should stop test execution

--- pkg/assert/helpers.go
+++ pkg/assert/helpers.go
@@ -6,21 +6,21 @@ import (
 
 func Error(t *testing.T, got error, want error) {
 	if got == nil {
-		t.Errorf("expected error, got nil")
+		t.Fatalf("expected error, got nil")
 	}
 	if got.Error() != want.Error() {
-		t.Errorf("expected error %q, got %q", want, got)
+		t.Fatalf("expected error %q, got %q", want, got)
 	}
 }
 
 func NoError(t *testing.T, got error) {
 	if got != nil {
-		t.Errorf("expected no error, got %v", got)
+		t.Fatalf("expected no error, got %v", got)
 	}
 }
 
 func EqualString(t *testing.T, got, want string) {
 	if got != want {
-		t.Errorf("expected %q, got %q", want, got)
+		t.Fatalf("expected %q, got %q", want, got)
 	}
 }
